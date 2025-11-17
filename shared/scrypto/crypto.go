d37aa104c8605c338810a2dc19d12bc274025103
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-11-17 16:25:07 +0700

    feat: add crypto and query utils in shared

---
+++ shared/scrypto/crypto.go
@@ -0,0 +1,19 @@
+package scrypto
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func RandomString(length int) string {
+	const (
+		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	)
+
+	b := make([]byte, length)
+	for i := range b {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		b[i] = charset[n.Int64()]
+	}
+	return string(b)
+}
